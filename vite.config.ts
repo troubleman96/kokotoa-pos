56097180fa8922da69e5511ad03138fd796386a0
troubleman <itslugenge96@gmail.com>
2026-02-18 01:55:47 +0300

    moremorechangeonapi

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'https://api-pos.kokotoa.online',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
       }
